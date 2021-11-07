6538f652158900a1cb2bc478d4d75a6e6a75b7cc
cumi2008 <rtda.cm@gmail.com>
2021-11-07 22:15:19 +0700

    BaiTapThuKinh

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import Card from './components/Card';
+import Profile from './components/Profile';
+import BaiTapComponent from './components/BaiTapComponent/BaiTapComponent';
+import BTVNComponent from './components/BaiTapComponent/BTVeNhaComponent-B1/BTVNComponent';
+import Databinding from './databinding/Databinding';
+import HandleEvent from './handleEvent/HandleEvent';
+import StateDemo from './state/StateDemo';
+import Style from './style/Style';
+import Example from './state/Example';
+import BTState from './state/BTState';
+import RenderWithMap from './renderWithMap/RenderWithMap';
+import DemoProps from './props/DemoProps';
+import BTThuKinh from './state/bTThuKinh/BTThuKinh';
+
+
+function App() {
+  return (
+    <div>
+      {/* <Nav/>
+      <Card/>
+
+      <Profile/>
+
+      <BaiTapComponent/> */}
+
+      {/* <BTVNComponent/> */}
+      {/* <Databinding/> */}
+      {/* <HandleEvent/> */}
+      {/* <StateDemo/> */}
+      {/* <Style/> */}
+      {/* <Example/> */}
+
+      {/* <BTState/> */}
+      {/* <RenderWithMap/> */}
+      {/* <DemoProps/> */}
+      <BTThuKinh/>
+
+    </div>
+    
+
+  );
+}
+
+export default App;
